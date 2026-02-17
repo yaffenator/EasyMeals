19437dfe23f09416ce13f9afa9ef4de40349e191
Wade <ayowadeowojori@gmail.com>
2026-02-17 15:38:28 -0800

    feature(db): add centralized Firestore client module

---
+++ backend/db/firestore_client.py
@@ -0,0 +1,11 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+cred_path = os.path.join(BASE_DIR, "secrets", "serviceAccountKey.json")
+
+cred = credentials.Certificate(cred_path)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
