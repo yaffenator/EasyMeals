0b911f12a6c12ee8df5fc995c223cf3b340be2ef
Wade <ayowadeowojori@gmail.com>
2026-02-17 18:25:45 -0800

    Made a more robust file path to the credentials

--- backend/db/firestore_client.py
+++ backend/db/firestore_client.py
@@ -8,4 +8,5 @@ cred_path = os.path.join(BASE_DIR, "secrets", "serviceAccountKey.json")
 cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
+#now backend has read/write access for the db, and you simply need to import this variable
 db = firestore.client()
